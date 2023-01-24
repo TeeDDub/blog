e8e572b5be109c393415c1f462a3db5bc1e4d0a7
teeddub <tracertw@gmail.com>
2023-01-24 20:17:02 +0900

    Move scss files to styles directory

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from "./Header";
-import style from "./Container.module.scss";
+import style from "@/styles/Container.module.scss";
 
 const Container = function(props:any) {
     return (
